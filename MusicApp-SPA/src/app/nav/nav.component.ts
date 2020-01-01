a47389279fd7383601426b49b74b77c22da8f6ed
mrjbowling <mrjbowling@gmail.com>
2020-01-01 09:48:47 -0500

    Add Error interceptor

--- MusicApp-SPA/src/app/nav/nav.component.ts
+++ MusicApp-SPA/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
       console.log('logged in');
     },
     error => {
-      console.log("Failed to login");
+      console.log(error);
     });
   }
   loggedIn() {
--- MusicApp.API/Startup.cs
+++ MusicApp.API/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.Extensions.Logging;
 using MusicApp.API.Data;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using MusicApp.API.Helpers;
 
 namespace MusicApp.API
 {
@@ -54,6 +58,21 @@ namespace MusicApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else 
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null) 
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             //app.UseHttpsRedirection();
