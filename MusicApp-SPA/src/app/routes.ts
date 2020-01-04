5867742bfcf78269478612185340b9dd0f4d3678
mrjbowling <mrjbowling@gmail.com>
2020-01-04 16:32:01 -0500

    protecting routes with route guard

--- MusicApp-SPA/src/app/routes.ts
+++ MusicApp-SPA/src/app/routes.ts
@@ -3,11 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages.component';
 import { LessonsComponent } from './lessons/lessons.component';
 import { PaymentComponent } from './payment/payment.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'lessons', component: LessonsComponent},
-    {path: 'messages', component: MessagesComponent},
-    {path: 'payment', component: PaymentComponent},
-    {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'lessons', component: LessonsComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'payment', component: PaymentComponent}
+        ]
+    },
+      {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
