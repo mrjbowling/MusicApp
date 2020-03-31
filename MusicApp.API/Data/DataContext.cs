2be22eec17310575112c32856fc006f8a9276dce
mrjbowling <mrjbowling@gmail.com>
2020-03-31 16:52:50 -0400

    deleting values controller

--- MusicApp.API/Data/DataContext.cs
+++ MusicApp.API/Data/DataContext.cs
@@ -9,8 +9,6 @@ namespace MusicApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base
         (options) {}
-        
-        public DbSet<Value> Values {get; set;}
 
         public DbSet<User> Users { get; set; }
         
